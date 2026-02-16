ebbe910ef6d5db9adf6a88315cad17d4007214a2
Narvin Sachaniya <githubnarvin@gmail.com>
2026-02-16 21:53:10 +0530

    feat: update push.js

--- backend/routes/push.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { subscribe } from '../controllers/pushController.js';
-
-const router = Router();
-
-router.post('/subscribe', subscribe);
-
-export default router;
