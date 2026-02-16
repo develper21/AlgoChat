4d9ad78e9d2a2b96ab8fe6464c6b2dce1941c8de
Narvin Sachaniya <githubnarvin@gmail.com>
2026-02-16 21:53:10 +0530

    feat: update FileUploadButton.jsx

--- frontend/src/components/FileUploadButton.jsx
+++
@@ -1,8 +0,0 @@
-const FileUploadButton = ({ onSelect }) => (
-  <label className="file-upload">
-    📎
-    <input type="file" onChange={(e) => onSelect(e.target.files?.[0] || null)} />
-  </label>
-);
-
-export default FileUploadButton;
