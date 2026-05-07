8f1d33e4dc28ad88900db88a17d31dad525dcc67
develper21 <githubnarvin@gmail.com>
2026-05-07 17:42:00 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
-import Navbar from "./components/Navbar";
-
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import MediaPage from "./pages/MediaPage";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
@@ -34,15 +33,14 @@ const App = () => {
     );
 
   return (
-    <div data-theme={theme}>
-      <Navbar />
-
+    <div data-theme={theme} className="h-screen">
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route path="/media" element={authUser ? <MediaPage /> : <Navigate to="/login" />} />
       </Routes>
 
       <Toaster />
